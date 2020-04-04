7b31803dfd9a82fe22b466dd3b06b3bb43ba8536
your_username <280279472@qq.com>
2020-04-05 00:19:35 +0800

    CORS跨域

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,11 +8,26 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+// import jsonp from 'jsonp'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
-  }
+  },
+  data() {
+    return {
+        age: 30
+    }
+  },
+  mounted() {
+    let url=" https://easy-mock.com/mock/5e88b16a6e7d856cbee19bc2/example_copy/logon"
+    axios.get(url).then(()=>{ })
+    // jsonp(url,()=>{
+
+    // })
+
+  },
 }
 </script>
